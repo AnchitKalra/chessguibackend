e14a0f1efe48b5477c25c59f1ba639ba20fabb98
Anchit Kalra <ak007kalra@gmail.com>
2024-02-20 11:21:55 +0000

    latest

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,11 +3,16 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 
 @SpringBootApplication
 public class App
 {
+    @RequestMapping("/")
+    public String home() {
+            return "Hello World!";
+    }
     public static void main( String[] args )
     {
         SpringApplication.run(App.class,args);
